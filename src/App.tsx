9aad4504d378a8e1fb501757cb1ea28209fbd5db
zil130 <55101149+zil130@users.noreply.github.com>
2023-06-14 22:02:03 -0300

    add: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import './App.css';
+import { styled } from 'styled-components';
+import Header from './components/Header';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  max-width: 800px;
+`;
 
 function App() {
   return (
-    <div className="App" />
+    <StyledApp>
+      <Header />
+    </StyledApp>
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { styled } from 'styled-components';
+
+const StyledTitle = styled.h1`
+  font-size: 70px;
+  font-weight: 100;
+  color: #e9d9d8;
+`;
+
+const Header = () => {
+  return (
+    <StyledTitle>todos</StyledTitle>
+  )
+}
+
+export default Header;
